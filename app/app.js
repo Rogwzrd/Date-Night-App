b6e266e69f591d197e68e85dbc5e7f9ca9c20012
Star Lee <zabra15@yahoo.com>
2017-09-29 17:03:52 -0400

    html for home, plus some css and js

---
+++ app/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.ui.dropdown')
+        .dropdown()
+    ;
+
+    $('.ui.range').range({
+        min: 0,
+        max: 10,
+        start: 5,
+        step: 1,
+    });
+
+
+
+
+});
+
