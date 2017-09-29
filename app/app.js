c93b09f720c644412d351ae2a422510041f33b22
unknown <mgdaye@gmail.com>
2017-09-29 14:44:38 -0400

    added basic html, background color, and returned yummly object

---
+++ app/app.js
@@ -0,0 +1,15 @@
+
+//search term for api
+var title = "pasta";
+//api query
+var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=d10c5b70&_app_key=af6e286e83053654370aa379046e6c3b&q=" + title;
+
+
+//yummly api
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).done(function(response) {
+    console.log(response);
+    $("#foodInfo").html(JSON.stringify(response));
+})
