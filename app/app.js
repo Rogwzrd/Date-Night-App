813551ca39f41d6f93515e92841cd1ee25b5d491
unknown <mgdaye@gmail.com>
2017-09-30 00:27:26 -0400

    added food object and omdb api

--- app/app.js
+++ app/app.js
@@ -1,23 +1,96 @@
+ var movies = {
+        action: {
+            "0": [],
+            "1": [],
+            "2": [],
+            "3": [],
+            "4": [],
+            "5": [],
+            "6": [],
+            "7": [],
+            "8": [],
+            "9": []
+        },
+        drama: {
+            "0": [],
+            "1": [],
+            "2": [],
+            "3": [],
+            "4": [],
+            "5": [],
+            "6": [],
+            "7": [],
+            "8": [],
+            "9": []
+        },
+        romCom: {
+            "0": [],
+            "1": [],
+            "2": [],
+            "3": [],
+            "4": [],
+            "5": [],
+            "6": [],
+            "7": [],
+            "8": [],
+            "9": []
+        },
+        thriller: {
+            "0": [],
+            "1": [],
+            "2": [],
+            "3": [],
+            "4": [],
+            "5": [],
+            "6": [],
+            "7": [],
+            "8": [],
+            "9": []
+        }
+    };
+    
+    var quisineVal = [],
+        dietaryVal = [],
+        includeVal = [],
+        excludeVal = [],
+        spicyVal = 0,
+        savoryVal = 0,
+        saltyVal = 0,
+        sweetVal = 0;
 
-//search term for api
-var title = "pasta";
-//api query
-var queryURL = "http://api.yummly.com/v1/api/recipes?_app_id=d10c5b70&_app_key=af6e286e83053654370aa379046e6c3b&q=" + title;
 
+    //search terms and key for api
+    var movieSearch = "dune";
+    var omdbKey = "40e9cece"
+    //omdb query url
+    var omdbQueryURL = "http://www.omdbapi.com/?t=" + movieSearch + "&apikey=" + omdbKey;
 
-//yummly api
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).done(function(response) {
-    console.log(response);
-    $("#foodInfo").html(JSON.stringify(response));
-})
+    //omdb api call
+    $.ajax({
+        url: omdbQueryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);
+    });
+
+    //search terms and key for api
+    var foodSearch = "pasta";
+    var yummlyKey = "af6e286e83053654370aa379046e6c3b"
+    //api query
+    var yummlyQueryURL = "http://api.yummly.com/v1/api/recipes?_app_id=d10c5b70&_app_key=" + yummlyKey + "&q=" + foodSearch;
+
+    //yummly api call
+    $.ajax({
+        url: yummlyQueryURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response);
+    })
+
 
 $(document).ready(function() {
     $('.ui.dropdown')
-        .dropdown()
-    ;
+        .dropdown();
 
     $('.ui.range').range({
         min: 0,
@@ -25,8 +98,5 @@ $(document).ready(function() {
         start: 5,
         step: 1,
     });
-
-
-
 
 });
