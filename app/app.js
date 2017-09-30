f8f75a28cbe834bc42436e6e46dfaa0ec4cc4d98
unknown <mgdaye@gmail.com>
2017-09-30 13:31:49 -0400

    changing branches

--- app/app.js
+++ app/app.js
@@ -136,26 +136,4 @@ $(document).ready(function() {
     });
 
 
-
-
-    //grabbing slider values
-
-    function sliderChange(val) {
-        document.getElementById("sliderStatus").innerHTML = val;
-        //document.getElementById("sliderStatus2").innerHTML=val;
-        //document.getElementById("sliderStatus3").innerHTML=val;
-    }
-
-    function sliderChange2(val) {
-        document.getElementById("sliderStatus2").innerHTML = val;
-    }
-
-    function sliderChange3(val) {
-        document.getElementById("sliderStatus3").innerHTML = val;
-    }
-
-    function sliderChange4(val) {
-        document.getElementById("sliderStatus4").innerHTML = val;
-    }
-
 });
